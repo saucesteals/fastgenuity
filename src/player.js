557b2540b386938be421c51a104e68f90c6c3984
daniel <saucesteals@gmail.com>
2021-02-22 12:14:13 -0800

    go to next lesson if possible, 'done' button now goes to next along with checking your answer'

--- src/player.js
+++ src/player.js
@@ -1,7 +1,19 @@
 window.fsG = window.fsG || {
   inited: false,
-  lastVideoSource: "",
   log: (message) => console.log("[fastgenuity] " + message),
+
+  next: () => {
+    if (API.FrameChain.framesStatus.length == API.FrameChain.currentFrame) {
+      fsG.log("attemping to go to the next activity")
+      return window.parent.document.querySelector(".goRight").click()
+    }
+
+    fsG.log("attemping to go to the next frame")
+    API.FrameChain.nextFrame()
+
+  },
+
+
   removeInvisDiv: () => {
     const elem = $("#invis-o-div");
     elem && elem.remove();
@@ -12,6 +24,22 @@ window.fsG = window.fsG || {
       elem.style = "";
     });
   },
+  
+  bindToDoneButton: () => {
+    const elem = document.querySelector("#btnCheck")
+    if (elem && !elem.fsG) {
+      elem.onclick = () => {
+        API.Frame.check(); 
+
+        setTimeout(() => {
+          if (API.Frame.isComplete()) {
+            fsG.next()
+          }
+        }, 500);
+      }
+      elem.fsG = true;
+    }
+  },
 
   MessageHandler: (e) => {
     setTimeout(fsG.startVideoTask, 6000);
@@ -19,18 +47,18 @@ window.fsG = window.fsG || {
   },
 
   startVideoTask: () => {
-    if (API.Video.video && API.Video.video.src !== fsG.lastVideoSource) {
+    if (API.Video.video && !API.Video.video.fsG) {
+      API.Video.video.fsG = true;
       fsG.log("starting video task");
-      fsG.lastVideoSource = API.Video.video.src;
 
       API.Video.video.addEventListener("progress", () => {
         fsG.log("attemping to skip on progress event");
-        API.FrameChain.nextFrame();
+        fsG.next();
       });
 
       API.Video.video.addEventListener("ended", () => {
         console.log("attemping to skip on ended event");
-        API.FrameChain.nextFrame();
+        fsG.next();
       });
       
     }
@@ -40,6 +68,7 @@ window.fsG = window.fsG || {
     fsG.log("starting question task");
     fsG.removeInvisDiv();
     fsG.showAllQuestions();
+    fsG.bindToDoneButton();
   },
 
   init: () => {
